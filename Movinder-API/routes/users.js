d6d3e26f499a08d6de26d1d03f6d75cc2c39def4
joshfernandes <joshfernandes@deloitte.com>
2020-09-20 23:28:47 +0530

    Knex changes and simple user verification

--- Movinder-API/routes/users.js
+++ Movinder-API/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+let {UserService} = require('../services/users.service');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.route('/').get( async (req, res, next) => {
   res.send('respond with a resource');
+});
+
+router.route('/check-user').post( async (req, res, next) => {
+  let { username, password} = req.body;
+  let user =await UserService.checkUser(username,password)
+  if(user){
+    res.send({'user_id':user});
+  } else {
+    res.status(404).json({
+      message: 'User not found'
+    });
+  }
+  
 });
 
 module.exports = router;
